3d728d0e11c064706ac85ac101c902d6dc8cf88e
kaywhy331 <140022536+kaywhy331@users.noreply.github.com>
2026-07-21 12:53:12 -0700

    let the experience overlay own chapter navigation

--- app/experience/layout.tsx
+++ app/experience/layout.tsx
@@ -1,6 +1,5 @@
 import { ExperienceShell } from '@/experience/ExperienceShell';
-import { PersistentTimelineNav } from '@/experience/PersistentTimelineNav';
 
 export default function ExperienceLayout({ children }: { children: React.ReactNode }) {
-  return <ExperienceShell><PersistentTimelineNav />{children}</ExperienceShell>;
+  return <ExperienceShell>{children}</ExperienceShell>;
 }
